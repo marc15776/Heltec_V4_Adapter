c7acfcec2a1f3cab5e2674a141cbd2ec63a359d9
marc15776 <langhart.marc2@gmail.com>
2026-04-16 19:22:28 +0200

    Halfway working code

--- Home_Node/src/main.cpp
+++ Home_Node/src/main.cpp
@@ -1,16 +1,23 @@
 #include <Arduino.h>
+#include <heltec.h>
 
 #define Boot_Key 0
 #define Led      35
+
+
 void setup() {
   // put your setup code here, to run once:
+  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
+  display.init();
   Serial.begin(115200);
   pinMode(Boot_Key, INPUT_PULLUP);
   pinMode(Led, OUTPUT);
+ 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  display->clear();
   if(digitalRead(Boot_Key)==0) {
     digitalWrite(Led,HIGH);
     Serial.print("hello world !!! \n");
